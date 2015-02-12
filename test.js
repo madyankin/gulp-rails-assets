8132de984b84f4610ba37a40664dd1943e418128
Alexander Madyankin <alexander@madyankin.name>
2015-02-12 21:39:16 +0600

    Add more tests and merging

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 'use strict';
 var path      = require('path');
-var crypto    = require('crypto');
 var fs        = require('fs');
 var assert    = require('assert');
 var gutil     = require('gulp-util');
 var helpers   = require('./lib/helpers');
+var fileData  = require('./lib/file');
+var manifest  = require('./lib/manifest');
 
 var sourceDir = './test';
-var assetsDir = './test/assets';
+var assetsDir = './test/assets/';
 var destDir   = './tmp';
 
 
@@ -25,18 +26,51 @@ it('respect directory structure', function() {
 });
 
 
-it('generates manifest', function() {
+it('builds correct file data', function() {
+  var filepath  = path.resolve(assetsDir + 'foo.css');
+  var mtime     = fs.statSync(filepath).mtime;
+
+  var file = new gutil.File({
+    path: filepath,
+    base: path.resolve(assetsDir),
+    contents: new Buffer('.foo {}\n')
+  });
+
+  var expected = {
+    newName:  'foo-a54dd39f86b3ef34bc57c1d778476f7f.css',
+    oldName:  'foo.css',
+    newPath:  path.resolve(assetsDir + 'foo-a54dd39f86b3ef34bc57c1d778476f7f.css'),
+    mtime:    mtime,
+    hash:     'a54dd39f86b3ef34bc57c1d778476f7f',
+    size:     8
+  }
+
+  assert.deepEqual(fileData(file), expected);
+});
+
+
+it('builds manifest', function() {
+  var files     = require('./test/files.json');
+  var expected  = require('./test/manifest.expected.json');
+  var res       = manifest(files).toJSON();
+  assert.deepEqual(res, expected);
+});
+
+
+it('saves manifest', function() {
   var files = fs.readdirSync(destDir);
   assert(files.indexOf('manifest.json') > -1);
 });
 
 
 it('merges manifest with existing one', function() {
-});
+  var files           = require('./test/files.json');
+  var filesMerge      = require('./test/files-merge.json');
+  var expected        = require('./test/manifest-merge.expected.json');
+  var manifestObject  = manifest(files);
+  var res             = manifestObject.merge(manifest(filesMerge))
 
-
-it('copies only changed files', function() {
-
+  assert.deepEqual(res, expected);
 });
 
 
